99e91db948212276e807f9b170dc2de0c1f62345
Enzo Gomes <110132900+enzogomezz@users.noreply.github.com>
2023-05-23 18:18:20 -0300

    Create App.tsx
    alterações que fiz no App.tsx

---
+++ stories/App.tsx
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { LoginPage } from "./pages/LoginPage.tsx";
+import { Home } from "./pages/Home.tsx";
+import { Users } from "./pages/Users.tsx";
+import { Rooms } from "./pages/Rooms.tsx";
+import { Events } from "./pages/Events.tsx";
+import { DashboardPage } from "./pages/DashboardPage.tsx";
+import { RegisterPage } from "./pages/RegisterPage.tsx";
+import { PassResetPage } from "./pages/PassResetPage.tsx";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/passreset" element={<PassResetPage />} />
+        <Route path="/dashboard" element={<DashboardPage />}>
+          <Route path="home" element={<Home />} />
+          <Route path="users" element={<Users />} />
+          <Route path="rooms" element={<Rooms />} />
+          <Route path="events" element={<Events />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
